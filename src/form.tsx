c13d72876f1c15fc06310be85f85b8da41b829a8
abdullahbhattidev <ab76293@gmail.com>
2024-01-22 00:30:47 +0500

    validation with react hook form

--- src/form.tsx
+++ src/form.tsx
@@ -1,34 +1,31 @@
 import { useRef, useState } from "react";
+import { useForm } from "react-hook-form";
+
+interface formData {
+    name: "",
+    age: number
+}
 
 function Form(){
-    // const name = useRef<HTMLInputElement>(null)
-    // const age = useRef<HTMLInputElement>(null)
-    // const person = {
-    //     name: "",
-    //     age: 0,
-    // }
-    const [person, setperson] = useState({
-        name: "",
-        age: 0
-    })
 
+     const {register, handleSubmit, formState: {errors}} = useForm <formData> ()
+     console.log(errors)
     return(
-        <form id="submit-form" action="submit" onSubmit={(e)=> {
-            e.preventDefault();
-            // if(name.current!== null)
-            // person.name = name.current?.value;
-            // if(age.current!== null)
-            // person.age = parseInt(age.current?.value);
-            console.log(person)
-        }}>
+        <form id="submit-form" action="submit" onSubmit={handleSubmit(e => console.log(e))}>
             <div className="inner-content">
             <div className="mb-3">
                 <label className="form-label" htmlFor="name">NAME</label>
-                <input value={person.name} onChange={(e) => setperson({...person, name: e.target.value})} id="name" className="form-control" type="text" />
+                <input {...register("name", {required: true, minLength: 5})} id="name" className="form-control" type="text" />
+                {errors.name?.type === "required" &&<p className="text-danger"> the field is required</p>}
+                {errors.name?.type === "minLength" &&<p className="text-danger">the text must be more than 5 characters</p>}
             </div>
+
             <div className="mb-3">
                 <label className="form-label" htmlFor="age">AGE</label>
-                <input value={person.age} onChange={(e) => setperson({...person, age: parseInt(e.target.value)})}  id="age" className="form-control" type="number" />
+                <input {...register("age",{required: true, min: 18, max: 40})}  id="age" className="form-control" type="number" />
+                {errors.age?.type === "required"  &&<p className="text-danger"> the field is required</p>}
+                {errors.age?.type === "min"  &&<p className="text-danger"> enter the age equals to or above 18</p>}
+                {errors.age?.type === "max"  &&<p className="text-danger"> enter the age equals to or below 40</p>}
             </div>
             <button className="btn btn-primary">Submit</button>
             </div>
@@ -38,3 +35,26 @@ function Form(){
 }
 
 export default Form
+
+//--------------old code----------
+// const name = useRef<HTMLInputElement>(null)
+    // const age = useRef<HTMLInputElement>(null)
+    // const person = {
+    //     name: "",
+    //     age: 0,
+    // }
+    // const [person, setperson] = useState({
+    //     name: "",
+    //     age: 0
+    // })
+
+      // (e)=> {
+            // e.preventDefault();
+            // if(name.current!== null)
+            // person.name = name.current?.value;
+            // if(age.current!== null)
+            // person.age = parseInt(age.current?.value);
+            // console.log(person)
+        // }
+        // <input value={person.name} onChange={(e) => setperson({...person, name: e.target.value})} id="name" className="form-control" type="text" />
+        // <input value={person.age} onChange={(e) => setperson({...person, age: parseInt(e.target.value)})}  id="age" className="form-control" type="number" />
