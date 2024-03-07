3e6bf14aadd41c2432012e7938ee8de1c679493f
abdullahbhattidev <ab76293@gmail.com>
2024-03-08 02:09:33 +0500

    handle errors in react query

--- src/todos.tsx
+++ src/todos.tsx
@@ -3,18 +3,19 @@ import axios from 'axios'
 import React from 'react'
 import { map } from 'zod'
 interface todos {
-    id?: number
+    title?: string
 }
 
-const Todos = ({id}: todos) => {
-    const {data} = useQuery({
+const Todos = ({title}: todos) => {
+    const {data, error} = useQuery<todos[], Error>({
         queryKey: ['todos'], 
         queryFn: () => axios.get<todos[]>("https://jsonplaceholder.typicode.com/todos")
                             .then(res => res.data)
     })
+    if(error) return <p>{error.message}</p>
 
   return (
-    <ul>{data?.map(todo => <li>{todo.id}</li>)}</ul>
+    <ul>{data?.map(todo => <li>{todo.title}</li>)}</ul>
   )
 }
 
