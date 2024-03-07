b37013ac0dbe2894ab65bcd5b7a31da4913233de
abdullahbhattidev <ab76293@gmail.com>
2024-03-08 02:13:06 +0500

    show loading indicator using react query

--- src/todos.tsx
+++ src/todos.tsx
@@ -7,13 +7,13 @@ interface todos {
 }
 
 const Todos = ({title}: todos) => {
-    const {data, error} = useQuery<todos[], Error>({
+    const {data, error, isLoading} = useQuery<todos[], Error>({
         queryKey: ['todos'], 
         queryFn: () => axios.get<todos[]>("https://jsonplaceholder.typicode.com/todos")
                             .then(res => res.data)
     })
     if(error) return <p>{error.message}</p>
-
+    if(isLoading) return <p>Loading...</p>
   return (
     <ul>{data?.map(todo => <li>{todo.title}</li>)}</ul>
   )
